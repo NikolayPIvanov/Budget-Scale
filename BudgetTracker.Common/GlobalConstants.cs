cdeaaf95988bb108a2bf7b8ae5cc6f9188cf978f
Nikolay Ivanov <nick.ivanov98@gmail.com>
2018-11-26 13:24:44 +0200

    Created common project and completed JWT configuration

---
+++ BudgetTracker.Common/GlobalConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BudgetTracker.Common
+{
+    public static class GlobalConstants
+    {
+        public const string AdministratorRoleName = "Administrator";
+
+        public const string JsonContentType = "application/json";
+    }
+}
