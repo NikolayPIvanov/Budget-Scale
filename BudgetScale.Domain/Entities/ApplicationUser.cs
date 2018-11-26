896e858a13e94081ebdcbc9b0e5f8b650a8be516
Nikolay Ivanov <nick.ivanov98@gmail.com>
2018-11-26 14:08:33 +0200

    Added ApplicationUser

--- BudgetScale.Domain/Entities/ApplicationUser.cs
+++ BudgetScale.Domain/Entities/ApplicationUser.cs
@@ -1,8 +1,24 @@
 namespace BudgetScale.Domain.Entities
 {
+    using System;
+    using BudgetScale.Domain.EntityContracts;
     using Microsoft.AspNetCore.Identity;
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser, IAuditInfo
     {
+        private bool _isLockedOut;
 
+        public string FullName { get; set; }
+
+        public bool IsLockedOut
+        {
+            get => _isLockedOut = this.LockoutEnabled && this.LockoutEnd < DateTime.UtcNow;
+            set => this._isLockedOut = value;
+        }
+
+
+        // Audit info
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+        public DateTime? ModifiedOn { get; set; }
     }
 }
