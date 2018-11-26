0c82e14f30d72722ec611df4efbc7ecdeaf5b3d2
Nikolay Ivanov <nick.ivanov98@gmail.com>
2018-11-26 12:18:40 +0200

    Removed server header and created Domain layer

--- BudgetScale.WebUI/Program.cs
+++ BudgetScale.WebUI/Program.cs
@@ -19,6 +19,7 @@ namespace BudgetScale.WebUI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            .UseKestrel(options => options.AddServerHeader = false)
                 .UseStartup<Startup>();
     }
 }
