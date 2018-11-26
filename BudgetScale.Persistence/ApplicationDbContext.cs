a4fdcfd22245d6b8b6c984b30146be99abb737ea
Nikolay Ivanov <nick.ivanov98@gmail.com>
2018-11-26 12:13:45 +0200

    Persistance layer

---
+++ BudgetScale.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace BudgetScale.Persistence
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+    }
+}
