263bbf1565fedec89a6721336a9d15216929cce7
Nikolay Ivanov <nick.ivanov98@gmail.com>
2018-12-01 11:43:22 +0200

    Get all groups command and controller action

--- BudgetScale.WebUI/Controllers/BaseController.cs
+++ BudgetScale.WebUI/Controllers/BaseController.cs
@@ -1,11 +1,21 @@
+
+using Microsoft.Extensions.Logging;
+
 namespace BudgetScale.WebUI.Controllers
 {
     using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using MediatR;
+
 
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    public abstract class BaseController : Controller
+    public abstract class BaseController: Controller
     {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ?? (_mediator = HttpContext.RequestServices.GetService<IMediator>());
     }
 }
---
+++ BudgetScale.WebUI/Controllers/GroupsController.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace BudgetScale.WebUI.Controllers
+{
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Threading.Tasks;
+
+    using Application.Groups.Models.Output;
+    using Application.Groups.Queries;
+    using Infrastructure.Extensions;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GroupsController : BaseController
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<GroupViewModel>), (int)HttpStatusCode.OK)]
+
+        public async Task<IActionResult> All([FromQuery] string month)
+        {
+            var response = await Mediator.Send(new GetGroupsQuery {Month = month, UserId = this.User.GetId()});
+
+            return Ok(response);
+        }
+    }
+}
