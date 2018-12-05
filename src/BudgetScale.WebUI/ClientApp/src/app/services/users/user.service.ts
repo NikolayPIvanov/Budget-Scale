c4de507a91bb650f3f5e71b6ff1074e7ea8cc05e
Nikolay Ivanov <nick.ivanov98@gmail.com>
2018-12-05 22:12:33 +0200

    login function

--- src/BudgetScale.WebUI/ClientApp/src/app/services/users/user.service.ts
+++ src/BudgetScale.WebUI/ClientApp/src/app/services/users/user.service.ts
@@ -3,7 +3,12 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { BehaviorSubject, Observable } from "rxjs";
 import { ConfigService } from "../config.service";
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 import { Injectable } from "@angular/core";
+import 'rxjs/Rx';
+import { map } from 'rxjs/operators';
+
+
 
 @Injectable({
     providedIn: 'root'
@@ -30,4 +35,29 @@ export class UserService extends BaseService {
         return this.http.post(this.baseUrl + "/accounts/register", body, { headers: headers, responseType: "text" })
     }
 
+    login(email, password) {
+        let body = new URLSearchParams();
+        body.set('email', email);
+        body.set('password', password);
+
+        return this.http.post(
+            this.baseUrl + '/accounts/login', body)
+            .pipe(map((response: any) => {
+                let tokenInfo = JSON.parse(response);
+                localStorage.setItem('auth_token', tokenInfo.auth_token);
+                this.loggedIn = true;
+                this._authNavStatusSource.next(true);
+                return true;
+            }));
+    }
+
+    logout() {
+        localStorage.removeItem('auth_token');
+        this.loggedIn = false;
+        this._authNavStatusSource.next(false);
+    }
+
+    isLoggedIn() {
+        return this.loggedIn;
+    }
 }
