c08fac86754615bbc85e78cb32cc05c0c104a5c3
Nikolay Ivanov <nick.ivanov98@gmail.com>
2018-12-06 15:51:22 +0200

    added register-form

--- src/BudgetScale.WebUI/ClientApp/src/app/modules/users/users.module.ts
+++ src/BudgetScale.WebUI/ClientApp/src/app/modules/users/users.module.ts
@@ -4,13 +4,14 @@ import { LoginFormComponent } from './login-form/login-form.component';
 import { FormsModule } from '@angular/forms';
 import { ConfigService } from 'src/app/services/config.service';
 import { UserService } from 'src/app/services/users/user.service';
+import { RegisterFormComponent } from '../register-form/register-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule
   ],
-  declarations: [LoginFormComponent],
+  declarations: [LoginFormComponent, RegisterFormComponent],
   providers: [
     ConfigService,
     UserService
